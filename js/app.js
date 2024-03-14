8c223489a5a075a5b3ec69a5e6ffce9d966c86bc
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-14 13:46:24 +0100

    ADD: array con numeri casuali

--- js/app.js
+++ js/app.js
@@ -13,23 +13,33 @@
 // con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe
+// non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora 
+// di rosso.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile
+//  di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente 
+// ha cliccato su una cella che non era una bomba.
+
+
 // collego bottone con js quando clicco si vedono tutte le celle
 let btnStart = document.querySelector('.btn-start'); // dom element
-console.log(btnStart)
+
 
 // creo collegamento con main-box dove inserirò le celle
 let boxCell = document.querySelector('.main-box')
 
 
-    
-    
-     
     // creo ascolto evento clic sul button gioca ora
     // da questo ascolto ne conseguono tutte le logiche 
     btnStart.addEventListener('click', function(){
         // creo collegamento con la select (quando l'utente clicca sul button, js legge la value della select)
         let interation = parseInt(document.getElementById('select').value); // dom element
-        console.log(select)
+        
         // dichiaro l'elemento del dom (dove appendo la griglia) stringa vuota
         // cosi se cambio valore della select mi genera una griglia nuova che sosrituisce la precedente
         boxCell.innerHTML='';
@@ -67,10 +77,31 @@ let boxCell = document.querySelector('.main-box')
     console.log ('cella cliccata n: ' + cell.innerHTML)
     })
 
-
+    
+    // creo un array vuoto che dovrà essere riempito di numeri
+    
     
 }
- 
+
+function generateRandomNumber(range){
+   let randomNumber = Math.floor(Math.random()* range)
+   return randomNumber
+}
+
+
+
+ const arrayBomb = []; // array da riempire
+ const numberOfBomb = 17;
+
+ for(let j = 1; j < numberOfBomb; j++){
+    let rangeBomb = generateRandomNumber(interation)
+    arrayBomb.push(rangeBomb)
+    
+       
+
+}
+
+console.log(arrayBomb)
 
 })
 
